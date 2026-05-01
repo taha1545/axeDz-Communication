449cbe59e034209d8d9b9a815319aea32047966b
taha1545 <senpaimato5@gmail.com>
2026-05-01 18:12:15 +0100

    sme fix and changes on endpoint logic

--- workers/email-worker/src/models/index.js
+++
@@ -1,8 +0,0 @@
-const sequelize = require('../config/db');
-const { DataTypes } = require('sequelize');
-const EmailLog = require('./EmailLog')(sequelize, DataTypes);
-
-module.exports = {
-  sequelize,
-  EmailLog,
-};
--- workers/sms-worker/src/models/index.js
+++
@@ -1,8 +0,0 @@
-const sequelize = require('../config/db');
-const { DataTypes } = require('sequelize');
-const SmsLog = require('./SmsLog')(sequelize, DataTypes);
-
-module.exports = {
-  sequelize,
-  SmsLog,
-};
